cf40c081bd78cda79ced8616eec97f2e1fb03918
Grant Paul <chpwn@chpwn.com>
2011-12-29 02:48:10 -0800

    Add Apply actions to Preferences.

--- Preferences.h
+++ Preferences.h
@@ -1,6 +1,9 @@
 
 #define kPreferencesBundleIdentifier @"com.chpwn.zephyr"
 
+typedef void (^PreferencesApplyAction)();
+
+void PreferencesApplyActionRegister(PreferencesApplyAction block);
 id PreferencesGet(NSString *key, id defaultValue);
 void PreferencesSet(NSString *key, id value);
 
