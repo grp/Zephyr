d79458b85b97674a13e36ea24c38e6749c206401
Grant Paul <chpwn@chpwn.com>
2012-05-11 21:16:26 -0700

    Move Common functions out of Switcher file.

---
+++ Common.h
@@ -0,0 +1,9 @@
+
+#import "iPhonePrivate.h"
+
+CGFloat ZephyrHeightForOrientation(UIInterfaceOrientation orientation);
+CGFloat ZephyrHeightFromScreenPercentage(CGFloat percentage, UIInterfaceOrientation orientation);
+
+void ZephyrRotateView(UIView *container, UIInterfaceOrientation currentOrientation);
+UIView *ZephyrViewWithScreenshotOfView(UIView *view);
+
