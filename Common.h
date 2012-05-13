bf68dd90bc35200ed6c6453070efaf420709517c
Grant Paul <chpwn@chpwn.com>
2012-05-13 12:54:27 -0700

    Fix bug with notifications and switcher; factor out bits of code into new Zephyr functions.

--- Common.h
+++ Common.h
@@ -5,6 +5,8 @@ CGFloat ZephyrHeightForOrientation(UIInterfaceOrientation orientation);
 CGFloat ZephyrHeightFromScreenPercentage(CGFloat percentage, UIInterfaceOrientation orientation);
 
 void ZephyrRotateViewFromOrientationToOrientation(UIView *view, UIInterfaceOrientation fromOrientation, UIInterfaceOrientation toOrientation, BOOL inPlace);
+
+UIView *ZephyrViewForApplication(SBApplication *app);
 UIView *ZephyrViewWithScreenshotOfView(UIView *view);
 
 UIInterfaceOrientation ZephyrOrientationFlip(UIInterfaceOrientation orientation);
